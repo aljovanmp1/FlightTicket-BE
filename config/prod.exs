ec96f4867efcb728164e4d326fb2107bbad43164
aljovanmp1 <141197702+aljovanmp1@users.noreply.github.com>
2024-10-26 15:51:25 +0700

    add prod port

--- config/prod.exs
+++ config/prod.exs
@@ -18,3 +18,13 @@ config :ticket_BE, Ticket_BE.Repo,
   ],
   stacktrace: true,
   show_sensitive_data_on_connection_error: true
+
+config :ticket_BE, Ticket_BEWeb.Endpoint,
+  # Binding to loopback ipv4 address prevents access from other machines.
+  # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
+  http: [ip: {0, 0, 0, 0}, port: 4000],
+  check_origin: false,
+  code_reloader: true,
+  debug_errors: true,
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
+  watchers: []
