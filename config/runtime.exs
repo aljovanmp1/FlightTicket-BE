bbbbecc7fc31f10d5d846e593808fbf657d15dc3
aljovanmp1 <141197702+aljovanmp1@users.noreply.github.com>
2024-10-26 21:34:22 +0700

    edit prod config

--- config/runtime.exs
+++ config/runtime.exs
@@ -24,17 +24,17 @@ if config_env() == :prod do
   database_url =
     System.get_env("DATABASE_HOSTNAME") ||
       raise """
-      environment variable DATABASE_URL is missing.
+      environment variable DATABASE_HOSTNAME is missing.
       For example: ecto://USER:PASS@HOST/DATABASE
       """
 
-  maybe_ipv6 = if System.get_env("ECTO_IPV6") in ~w(true 1), do: [:inet6], else: []
+  # maybe_ipv6 = if System.get_env("ECTO_IPV6") in ~w(true 1), do: [:inet6], else: []
 
-  config :ticket_BE, Ticket_BE.Repo,
-    # ssl: true,
-    url: database_url,
-    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-    socket_options: maybe_ipv6
+  # config :ticket_BE, Ticket_BE.Repo,
+  #   # ssl: true,
+  #   url: database_url,
+  #   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  #   socket_options: maybe_ipv6
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
